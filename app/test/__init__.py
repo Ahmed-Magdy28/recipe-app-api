0f88b55cab4714e94af9363e9e64ad7d11dcce28
Ahmed-Magdy28 <ray2legand@gmail.com>
2024-09-23 17:32:24 +0300

    aded a test

---
+++ app/test/__init__.py
@@ -0,0 +1,1 @@
+# This is to make Python treat the directory as a package
---
+++ app/test/test_example.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+
+class ExampleTest(TestCase):
+    def test_basic_addition(self):
+        """Test that 1 + 1 equals 2."""
+        self.assertEqual(1 + 1, 2)
+
+
+class ExampleTest1(TestCase):
+    def test_basic_addition(self):
+        """Test that 1 + 1 equals 2."""
+        self.assertEqual(1 + 1, 2)
+
+
+class ExampleTest2(TestCase):
+    def test_basic_addition(self):
+        """Test that 1 + 1 equals 2."""
+        self.assertEqual(1 + 1, 2)
+
+
+class ExampleTest3(TestCase):
+    def test_basic_addition(self):
+        """Test that 1 + 1 equals 2."""
+        self.assertEqual(1 + 1, 2)
