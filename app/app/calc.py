b4de4d0600066bb47a87bf5e3f9f8d738b8bd2f1
Ahmed-Magdy28 <ray2legand@gmail.com>
2024-09-29 21:49:17 +0300

    test worked

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""Calculator function"""
+
+
+def add(x: int, y: int) -> int:
+    """ add x and y return result"""
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    "Subtract x and y return result"
+    return x - y
--- app/test/test_example.py
+++ app/test/test_example.py
@@ -1,25 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from app import calc
 
 
-class ExampleTest(TestCase):
-    def test_basic_addition(self):
-        """Test that 1 + 1 equals 2."""
-        self.assertEqual(1 + 1, 2)
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        self.assertEqual(calc.add(5, 6), 11)
+
+    def test_subtract_numbers(self):
+        "Test subtract"
+        self.assertEqual(calc.subtract(5, 5), 0)
 
 
 class ExampleTest1(TestCase):
-    def test_basic_addition(self):
-        """Test that 1 + 1 equals 2."""
-        self.assertEqual(1 + 1, 2)
-
-
-class ExampleTest2(TestCase):
-    def test_basic_addition(self):
-        """Test that 1 + 1 equals 2."""
-        self.assertEqual(1 + 1, 2)
-
-
-class ExampleTest3(TestCase):
     def test_basic_addition(self):
         """Test that 1 + 1 equals 2."""
         self.assertEqual(1 + 1, 2)
